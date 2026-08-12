fd6b8a8ae8e4f756b821489a3bfba40710c5f805
Austin Horn <91858347+austinmhorn@users.noreply.github.com>
2026-08-11 23:05:08 -0500

    Add deterministic random generator

---
+++ core/include/basilisk/Random.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+#include <random>
+
+namespace basilisk {
+
+class RandomGenerator {
+public:
+    explicit RandomGenerator(std::uint64_t seed);
+
+    [[nodiscard]] int range(int minInclusive, int maxInclusive);
+    [[nodiscard]] bool chance(std::uint32_t numerator, std::uint32_t denominator);
+
+private:
+    std::mt19937_64 engine_;
+};
+
+} // namespace basilisk
