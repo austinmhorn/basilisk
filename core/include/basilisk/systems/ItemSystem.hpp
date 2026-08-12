e20f6d67209ac6e40a5d14d84727bacba30ea0ed
Austin Horn <91858347+austinmhorn@users.noreply.github.com>
2026-08-12 13:32:57 -0500

    Extend item system for world utility items

--- core/include/basilisk/systems/ItemSystem.hpp
+++ core/include/basilisk/systems/ItemSystem.hpp
@@ -2,7 +2,9 @@
 
 #include <vector>
 
+#include "basilisk/Action.hpp"
 #include "basilisk/Event.hpp"
+#include "basilisk/MatchState.hpp"
 #include "basilisk/Player.hpp"
 #include "basilisk/Rules.hpp"
 #include "basilisk/items/Item.hpp"
@@ -15,6 +17,11 @@ public:
         PlayerState& player,
         ItemType item,
         const Rules& rules);
+
+    [[nodiscard]] static std::vector<GameEvent> use(
+        MatchState& state,
+        PlayerState& player,
+        const PlayerAction& action);
 };
 
 } // namespace basilisk
