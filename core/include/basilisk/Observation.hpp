4984c1ee14ebe55387fbdfe82667aa00e6a54217
Austin Horn <91858347+austinmhorn@users.noreply.github.com>
2026-08-12 09:49:44 -0500

    Add player-specific observation model

---
+++ core/include/basilisk/Observation.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <optional>
+
+#include "basilisk/Types.hpp"
+#include "basilisk/actors/Basilisk.hpp"
+#include "basilisk/items/Item.hpp"
+
+namespace basilisk {
+
+enum class ObservationType {
+    RivalNearby,
+    PitNearby,
+    JackalNearby,
+    BasiliskNearby,
+    BasiliskNearbySubtle,
+    RestlessBasiliskNoise,
+    EnragedLastKnownCave,
+
+    ArrowHitYou,
+    YouWereDamaged,
+    YouKilledRival,
+    YouDied,
+    ItemFound,
+    ArrowFound,
+    ExoticCallingCardFound,
+    SigilAcquired,
+    ExtractionRevealed,
+    EscapeAvailable,
+    BasiliskEvaded,
+    BasiliskBehaviorChanged,
+    BasiliskKilled,
+    MatchDrawn
+};
+
+struct PlayerObservation {
+    ObservationType type{};
+    PlayerId viewer{};
+    std::optional<CaveId> cave;
+    std::optional<PlayerId> otherPlayer;
+    int amount{0};
+    std::optional<BasiliskBehavior> basiliskBehavior;
+    std::optional<ItemType> itemType;
+};
+
+} // namespace basilisk
